d23a17a061b154b98d16030b34e5f612101b22c8
blessli <ldm112358@163.com>
2022-11-23 18:45:05 +0800

    fix

--- UDPClient.c
+++ UDPClient.c
@@ -7,43 +7,48 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-	
-#define PORT	 9991
+
+#define PORT 9991
 #define MAXLINE 1024
-	
+
 // Driver code
-int main() {
+int main()
+{
 	int sockfd;
 	char buffer[MAXLINE];
 	char *hello = "Hello from client";
-	struct sockaddr_in	 servaddr;
-	
+	struct sockaddr_in servaddr;
+
 	// Creating socket file descriptor
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
-	
+
 	memset(&servaddr, 0, sizeof(servaddr));
-		
+
 	// Filling server information
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(PORT);
 	servaddr.sin_addr.s_addr = INADDR_ANY;
-		
+
 	int n, len;
-		
-	sendto(sockfd, (const char *)hello, strlen(hello),
-		MSG_CONFIRM, (const struct sockaddr *) &servaddr,
-			sizeof(servaddr));
+
+	for (int i = 0; i < 3; i++)
+	{
+		int ans = sendto(sockfd, (const char *)hello, strlen(hello),
+			   MSG_CONFIRM, (const struct sockaddr *)&servaddr,
+			   sizeof(servaddr));
+	}
 	printf("Hello message sent.\n");
-			
+
 	n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-				MSG_WAITALL, (struct sockaddr *) &servaddr,
-				&len);
+				 MSG_WAITALL, (struct sockaddr *)&servaddr,
+				 &len);
 	buffer[n] = '\0';
 	printf("Server : %s\n", buffer);
-	
+
 	close(sockfd);
 	return 0;
 }
